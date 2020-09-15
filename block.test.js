04c8f8814e0e5984d9189ec266d4e264c7bfd441
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-09-15 11:57:04 -0400

    (feat) Add ability to create a genesis block

--- block.test.js
+++ block.test.js
@@ -1,4 +1,5 @@
 const Block = require('./block');
+const { GENESIS_DATA } = require('./config.js');
 
 describe('Block', () => {
   const timestamp = 'a-date';
@@ -17,5 +18,17 @@ describe('Block', () => {
     expect(block.lastHash).toEqual(lastHash);
     expect(block.hash).toEqual(hash);
     expect(block.data).toEqual(data);
+  });
+
+  describe('genesis()', () => {
+    const genesisBlock = Block.genesis();
+
+    it('returns a Blockl instance', () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it('returns the genesis data', () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
   });
 });
