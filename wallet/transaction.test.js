9d1a2e5bcd2c5056f0f6f71261e5e093b07067e6
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-09-21 16:50:11 -0400

    (feat) Add the ability to update existing recipient within same transaction if needed

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -95,7 +95,7 @@ describe('Transaction', () => {
     });
   });
 
-  describe('updaate()', () => {
+  describe('update()', () => {
     let originalSignature, originalSenderOutput, nextRecipient, nextAmount;
 
     describe('and the amount is invalid', () => {
@@ -144,6 +144,31 @@ describe('Transaction', () => {
 
       it('re-signs the transaction', () => {
         expect(transaction.input.signature).not.toEqual(originalSignature);
+      });
+
+      describe('and another update for the same recipient', () => {
+        let addedAmount;
+
+        beforeEach(() => {
+          addedAmount = 80;
+          transaction.update({
+            senderWallet,
+            recipient: nextRecipient,
+            amount: addedAmount,
+          });
+        });
+
+        it('adds to the recipient amount', () => {
+          expect(transaction.outputMap[nextRecipient]).toEqual(
+            nextAmount + addedAmount
+          );
+        });
+
+        it('subtracts the amount from the original sender output amount', () => {
+          expect(transaction.outputMap[senderWallet.publicKey]).toEqual(
+            originalSenderOutput - nextAmount - addedAmount
+          );
+        });
       });
     });
   });
