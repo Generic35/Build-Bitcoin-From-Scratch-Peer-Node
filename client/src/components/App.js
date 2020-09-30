2eff7e02ecf0c2bceafa0e9170db60b6cf40d57f
Eric Lamontagne <eric.lamontagne@mphasis.com>
2020-09-30 08:45:42 -0400

    (feat) Add body style and logo

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Blocks from './Blocks';
+import logo from '../assets/logo.png';
 
 class App extends Component {
   state = { walletInfo: {} };
@@ -17,7 +18,10 @@ class App extends Component {
 
     return (
       <div>
+        <img className="logo" src={logo}></img>
+        <br />
         <div>Welcome to the blockchain...</div>
+        <br />
         <div>Address: {address}</div>
         <div>Balance: {balance}</div>
         <br />
